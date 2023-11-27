7e0e813dcb7006acb0c270c620431c527791e0c1
ogneyar <ya13th@mail.ru>
2023-11-27 20:09:48 +0300

    #autocommit

---
+++ CMakeProject/CMakeProject.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
