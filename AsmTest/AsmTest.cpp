541f5a09bffac46c3a779d98465c520cd3545f94
ogneyar <ya13th@mail.ru>
2023-11-28 11:29:06 +0300

    #autocommit

---
+++ AsmTest/AsmTest.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+#include <windows.h>
+
+extern "C" int testproc(int one, int two);
+
+int main()
+{
+	int a;
+
+	a = testproc(2, 5);
+
+    std::cout << "Hello World!\n";
+	std::cout << "\n" << a << "\n\n";
+
+	Sleep(5000);
+}
+
--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 
 ## Visual Studio projects
+gt
